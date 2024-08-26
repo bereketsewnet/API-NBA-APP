b24e523950b973af466c9be7f4a74b8ea61a80de
bereketsewnet <bereketsewnet@users.noreply.github.com>
2024-08-27 02:20:56 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/nba_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nba_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:nba_app/home_page.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
